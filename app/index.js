27f0dfd8d7712f0b7c0899ff601645b8847a17b5
Brijen Makwana <brijenmakwana@192.168.1.6>
2023-03-14 10:47:32 +0530

    Banner

--- app/index.js
+++ app/index.js
@@ -1,11 +1,15 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, View } from "react-native";
 import React from "react";
 import SearchBar from "../components/SearchBar";
+import Banner from "../components/Banner";
 
 const index = () => {
   return (
     <SafeAreaView style={styles.container}>
       <SearchBar />
+      <View style={styles.bannerContainer}>
+        <Banner />
+      </View>
     </SafeAreaView>
   );
 };
@@ -15,5 +19,8 @@ export default index;
 const styles = StyleSheet.create({
   container: {
     marginTop: 50,
+  },
+  bannerContainer: {
+    paddingVertical: 15,
   },
 });
---
+++ components/Banner.js
@@ -0,0 +1,28 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Banner = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../assets/Images/banner1.jpg")}
+        style={styles.image}
+      />
+    </View>
+  );
+};
+
+export default Banner;
+
+const styles = StyleSheet.create({
+  container: {
+    width: "90%",
+    alignSelf: "center",
+    height: 200,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+});
