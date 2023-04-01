f4780b1d786c333d14437d48452cb0475fead407
Brijen Makwana <brijenmakwana@192.168.1.6>
2023-04-01 16:15:28 +0530

    active category style changes

--- app/ProductsScreen.js
+++ app/ProductsScreen.js
@@ -1,14 +1,25 @@
 import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { productsCategories } from "../assets/data/data";
 import SidebarProduct from "../components/SidebarProduct";
 
 const ProductsScreen = () => {
+  const [activeCategory, setActiveCategory] = useState("Fresh Vegetables");
+
+  const changeProductCategory = (category) => {
+    setActiveCategory(category);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.sideBar} showsVerticalScrollIndicator={false}>
         {productsCategories.map((product) => (
-          <SidebarProduct key={product.id} {...product} />
+          <SidebarProduct
+            key={product.id}
+            {...product}
+            changeProductCategory={changeProductCategory}
+            activeCategory={activeCategory}
+          />
         ))}
       </ScrollView>
     </SafeAreaView>
