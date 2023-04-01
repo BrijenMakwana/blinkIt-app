6a33e38fce162d40f9519efe5c044ff5afc1fb61
Brijen Makwana <brijenmakwana@192.168.1.6>
2023-04-01 16:46:19 +0530

    products in ProductsScreen

--- app/ProductsScreen.js
+++ app/ProductsScreen.js
@@ -1,7 +1,14 @@
-import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  View,
+  FlatList,
+} from "react-native";
 import React, { useState } from "react";
-import { productsCategories } from "../assets/data/data";
+import { productsCategories, vegItems } from "../assets/data/data";
 import SidebarProduct from "../components/SidebarProduct";
+import ProductItem from "../components/ProductItem";
 
 const ProductsScreen = () => {
   const [activeCategory, setActiveCategory] = useState("Fresh Vegetables");
@@ -12,6 +19,7 @@ const ProductsScreen = () => {
 
   return (
     <SafeAreaView style={styles.container}>
+      {/* left side */}
       <ScrollView style={styles.sideBar} showsVerticalScrollIndicator={false}>
         {productsCategories.map((product) => (
           <SidebarProduct
@@ -22,6 +30,25 @@ const ProductsScreen = () => {
           />
         ))}
       </ScrollView>
+
+      {/* right side */}
+      <View style={styles.productList}>
+        <View style={styles.productList}>
+          <FlatList
+            data={vegItems}
+            renderItem={({ item }) => (
+              <ProductItem
+                {...item}
+                openProductModal={undefined}
+                imageBorder={false}
+              />
+            )}
+            numColumns={2}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+          />
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
@@ -32,8 +59,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 50,
+    flexDirection: "row",
   },
   sideBar: {
-    width: 80,
+    maxWidth: 80,
+    // backgroundColor: "red",
+  },
+  productList: {
+    backgroundColor: "rgb(244,246,251)",
+    flex: 1,
   },
 });
